00de4320250ac1592376e8b07cc401ea247f4ea7
jessjazz <54814522+jessjazz@users.noreply.github.com>
2020-11-27 16:47:00 +0000

    Update TankGameModeBase.h
    Added start delay.

--- TankGameModeBase.h
+++ TankGameModeBase.h
@@ -8,6 +8,7 @@
 
 class APawnTurret;
 class APawnTank;
+class APlayerControllerBase;
 
 UCLASS()
 class TOONTANKS_API ATankGameModeBase : public AGameModeBase
@@ -18,6 +19,7 @@ private:
 
 	APawnTank* PlayerTank;
 	int32 TargetTurrets = 0;
+	APlayerControllerBase* PlayerControllerRef;
 
 	int32 GetTargetTurretCount();
 	void HandleGameStart();
@@ -28,6 +30,9 @@ public:
 	void ActorDied(AActor* DeadActor);
 
 protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Game Loop")
+	int32 StartDelay = 3;
 
 	virtual void BeginPlay() override;
 	UFUNCTION(BlueprintImplementableEvent)
